b5d3baa9307c1f1a6d011bb30e3ce017c1cc902d
Marti-code <marti.huza@gmail.com>
2023-09-13 20:15:22 +0200

    server.js documentation

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Import required modules and set up environment variables
 const express = require("express");
 const app = express();
 
@@ -10,6 +11,9 @@ const secretKey = process.env.SECRET_KEY;
 
 const mongoose = require("mongoose");
 
+/**
+ * Connect to the mongodb database using mongoose
+ */
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_CONNECT, {
@@ -24,6 +28,7 @@ const connectDB = async () => {
 };
 connectDB();
 
+// Set view engine, configure middleware, and initialize token variable
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -31,52 +36,73 @@ app.use(express.static("public"));
 
 let token = "";
 
+// Define routes and their functionality
+
+/**
+ * Generate a json web token for the user's browser
+ * @returns {string} - A web token in JSON format
+ */
 app.post("/authenticate", (req, res) => {
   const payload = {
     key: Date.now().toString(),
   };
+
   token = jwt.sign(payload, secretKey);
-  console.log(token);
+
   res.json({ token });
 });
 
-//load the website and the url codes
+/**
+ * Render the main site (index.ejs)
+ */
 app.get("/", async (req, res) => {
   res.render("index");
 });
 
-//return the user's links based on the key
+/**
+ * Fetch the user's shortened links from the database based on userKey
+ * @returns {string} - user's shortened links in JSON format
+ */
 app.get("/api/shortened-links", async (req, res) => {
-  const userKey = req.query.userKey; // Retrieve the user's key from the query parameters
+  // Retrieve the user's key from the query parameters
+  const userKey = req.query.userKey;
 
-  // Fetch the user's shortened links from the database based on userKey
   const userShortenedLinks = await ShortUrl.find({ userKey: userKey });
 
-  // Send the user's shortened links as a JSON response
   res.json(userShortenedLinks);
 });
 
-//create a short link and save it in the database
+/**
+ * Create and save in db a short link using userKey (json web token) and full url
+ */
 app.post("/shortUrls", async (req, res) => {
   await ShortUrl.create({
     userKey: req.body.key,
     full: req.body.full,
   });
 
+  // go back to the main page
   res.redirect("/");
 });
 
+/**
+ * Redirect a user using a short link to the proper website
+ */
 app.get("/:shortUrl", async (req, res) => {
-  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+  // find the url based on the short url in the link's parameters
+  const requestedUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
 
-  if (shortUrl == null) {
+  // if the url doesn't exist send a 404 status
+  if (requestedUrl == null) {
     return res.sendStatus(404);
   }
 
-  shortUrl.clicks++;
-  shortUrl.save();
+  // Increment the number of visits on the website by one
+  requestedUrl.clicks++;
+  requestedUrl.save();
 
-  res.redirect(shortUrl.full);
+  res.redirect(requestedUrl.full);
 });
 
+// Listen on the specified port or 5000
 app.listen(process.env.PORT || 5000, () => console.log("Running"));
